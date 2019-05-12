40ab331e608a944e9d40b82759ad2b156011f7fa
luiz-junior <junior.rodrigues0222@gmail.com>
2019-05-12 13:33:48 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { createMuiTheme } from '@material-ui/core/styles';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import './App.css';
+import Header from './common/Header';
+import Home from './home';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { 
+      main: '#e91e63'
+    },
+    secondary: {
+      main: '#00b0ff',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <Header />
+      <Home />
+    </MuiThemeProvider>
   );
 }
 
---
+++ src/home/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Divider from '@material-ui/core/Divider';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingBasket, faCheck } from '@fortawesome/free-solid-svg-icons';
+import './home.css';
+
+const Home = props => {
+    return (
+        <div className="page-container">
+
+            <div className="new-list-container">
+                <Card className="card">
+                    <CardActionArea className="card-action-area">
+                        <CardContent className="card-content">
+                            <div>
+                                <p className="title">Adicionar novas Listas</p>
+                            </div>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            </div>
+
+            <div className="list-container">
+                <Card className="card">
+                    <CardActionArea className="card-action-area">
+                        <CardContent className="card-content">
+                            <div>
+                                <p className="title">Mês</p>
+                                <div className="list-card-body">
+                                    <div className="list-card-item">
+                                        <FontAwesomeIcon icon={faShoppingBasket} size="sm" />
+                                        <p> 1 Item(s) restante</p>
+                                    </div>
+                                    <div className="list-card-item">
+                                    <FontAwesomeIcon icon={faCheck} size="sm" />
+                                        <p> 2 Item(s) comprados</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </CardContent>
+                    </CardActionArea>
+                    <Divider />
+                    <CardActions className="card-footer">
+                        <p>12/05/2019</p>
+                        <p>100.00 R$</p>
+                    </CardActions>
+                </Card>
+            </div>
+
+        </div>
+    );
+}
+
+export default Home;
